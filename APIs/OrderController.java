ab6985344a86403f6112a574889b96096011ab1f
Stan <stasostaszewski@gmail.com>
2022-06-05 10:43:16 +0200

    Added useless scripts

---
+++ APIs/OrderController.java
@@ -0,0 +1,13 @@
+package APIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderController {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
